75ae5b832435afe44da9c2a9f9e70476e0f29f8a
hiro881213 <nikep.godbard@gmail.com>
2024-09-15 17:52:25 +0900

    文字列のスライスを追加する

--- chapter1_5.py
+++ chapter1_5.py
@@ -6,3 +6,10 @@ print(word[1])
 
 print("##### 文字列の最後を出力する #####")
 print(word[-1])
+
+print("##### 文字列のスライス #####")
+print(word[0:2])
+print(word[2:5])
+print(word[:2])
+print(word[2:])
+print(word[:])
