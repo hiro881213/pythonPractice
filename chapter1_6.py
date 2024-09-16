42e03084e14ef1cb63c02f13322cf08a973223b6
hiro881213 <nikep.godbard@gmail.com>
2024-09-16 21:53:45 +0900

    find methodを追加する

--- chapter1_6.py
+++ chapter1_6.py
@@ -1,7 +1,11 @@
-print('##### StartsWith function ######')
 s = 'My name is Mike. Hi Mike.'
+
+print('##### StartsWith function ######')
 is_start = s.startswith('My')
 print(is_start)
 
 is_start2 = s.startswith('X')
 print(is_start2)
+
+print('##### find method #####')
+print(s.find('Mike'))
