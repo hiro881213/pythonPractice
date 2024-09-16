6103bdc6c643bb8fcb11a6022f98e1b0afa7e97b
hiro881213 <nikep.godbard@gmail.com>
2024-09-16 22:14:14 +0900

    format method(Del warnings)を追加する

--- chapter1_7.py
+++ chapter1_7.py
@@ -1,4 +1,9 @@
-print('##### format method #####')
 s = 'a is {}'
+
+print('##### format method #####')
 print(s.format('a'))
 print(s.format('test'))
+
+print('##### format method(Del warnings) #####')
+a = 'test'
+print(f"a is {a}")
